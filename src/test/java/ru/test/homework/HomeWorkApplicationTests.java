42f13eadb230b907149efc7963eb4f01e2d69fdb
a1warez <joeclab2@gmail.com>
2025-06-13 15:22:04 +0300

    Comit

---
+++ src/test/java/ru/test/homework/HomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.test.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
