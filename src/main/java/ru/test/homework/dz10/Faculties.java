6496e61024b598292ff22f91630a568114dc336f
a1warez <joeclab2@gmail.com>
2025-06-27 15:35:29 +0300

    dz10

---
+++ src/main/java/ru/test/homework/dz10/Faculties.java
@@ -0,0 +1,26 @@
+package ru.test.homework.dz10;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@Table(name = "Faculties")
+public class Faculties {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private int id;
+
+    @NotNull(message = "Financing cannot be null")
+    @DecimalMin(value = "0.0", message = "Financing must be greater than or equal to 0")
+    @Column(name = "Financing")
+    private Double financing;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Column(name = "Name", unique = true)
+    private String name;
+
+}
